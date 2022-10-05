62f0ea8cc30824a115927d44394208e99d780b49
Jacob Odle <jodle@optimaxsi.com>
2022-10-05 14:10:55 -0400

    still doesn't work.

--- descartes_moveit/include/descartes_moveit/test_object.h
+++ descartes_moveit/include/descartes_moveit/test_object.h
@@ -3,14 +3,13 @@
 namespace descartes_moveit {
 
     class TestClass {
-
-        public:
-        TestClass();
-        ~TestClass();
-
+    public:
+        TestClass(int value) {
+            test_var = value;
+        }
         int checkTestVar();
 
-        private:
+    private:
 
         int test_var{};
         double test_var2{};
